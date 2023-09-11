a68cc3068d9f5cbb9fe0833c9bf9fdba5cc1c81b
binn328 <binn328@gmail.com>
2023-09-11 22:36:33 +0900

    add basic route

--- schedule_management_web/src/routes/+page.svelte
+++ schedule_management_web/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<head>
+    <meta charset="UTF-8">
+    <title>일정관리 로그인</title>
+</head>
+
+<body>
+    <div style="text-align: center; font-family: 'Malgun Gothic';"><h1>로그인</h1></div>
+    <hr>
+
+    <form method="post" action="login">
+        <table style="margin-left: auto; margin-right: auto">
+            <tr>
+                <td style="font-family: 'Malgun Gothic'">아이디: </td>
+                <td><input type="text" name="user_id"></td>
+            </tr>
+            <tr>
+                <td style="font-family: 'Malgun Gothic">비밀번호: </td>
+                <td><input type="text" name="user_password"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <button type="submit">로그인</button>
+                    <button type="button"><a href="/signup">회원가입</a></button>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
